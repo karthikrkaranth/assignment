2bf808b17c15fa13b1aa595191da94bb69519d5e
karthik <karthikrkaranth@gmail.com>
2016-06-07 12:57:41 +0000

    All new files are added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,12 +22,12 @@ Rails.application.configure do
   
 config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'http://peaceful-ocean-80983.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://peaceful-ocean-80983.herokuapp.com/' }
  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name:      'karanthrkarthik@gmail.com',
     password:       'tenniskrishna3',
-    domain:         'http://peaceful-ocean-80983.herokuapp.com/',
+    domain:         'gmail.com',
     address:       'smtp.gmail.com',
     port:          '587',
     authentication: :plain,
