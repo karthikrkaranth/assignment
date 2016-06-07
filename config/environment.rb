0eab4724c5803e692c6f8bd2c4ef2affbb226ed3
karthik <karthikrkaranth@gmail.com>
2016-06-07 06:19:36 +0000

    added

--- config/environment.rb
+++ config/environment.rb
@@ -3,14 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    user_name:      'karanthrkarthik@gmail.com',
-    password:       'tenniskrishna3',
-    domain:         'gmail.com',
-    address:       'smtp.gmail.com',
-    port:          '587',
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
