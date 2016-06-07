ee25ec225f5aa2ebcb0fe0ee67d704997922a1f7
karthik <karthikrkaranth@gmail.com>
2016-06-07 05:42:03 +0000

    mail added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,10 +25,10 @@ config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'https://peaceful-ocean-80983.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name:      'karanthrkarthik@gmail.com',
-    password:       'tenniskrishna3',
-    domain:         'gmail.com',
-    address:       'smtp.gmail.com',
+    user_name:       ENV['SENDGRID_USERNAME'],
+    password:       ENV['SENDGRID_PASSWORD'],
+    domain:         'heroku.com',
+    address:      'smtp.sendgrid.net',
     port:          '587',
     authentication: :plain,
     enable_starttls_auto: true
