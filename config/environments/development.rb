e0f47314dacf82587e468c1a47e8ad3463f98817
karthik <karthikrkaranth@gmail.com>
2016-06-08 04:20:20 +0000

    p changed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,7 @@ config.action_mailer.perform_deliveries = true
  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name:      'karanthrkarthik@gmail.com',
-    password:       'tenniskrishna3',
+    password:       'tenniskrishna4',
     domain:         'gmail.com',
     address:       'smtp.gmail.com',
     port:          '587',
