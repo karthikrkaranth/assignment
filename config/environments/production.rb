2f239bd38b5ddd43f0cf8a300b2fd0f2063e9fbe
karthik <karthikrkaranth@gmail.com>
2016-06-08 03:39:33 +0000

    production file added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
 Rails.application.routes.default_url_options[:host] = 'https://peaceful-ocean-80983.herokuapp.com/'
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = false
+config.action_mailer.raise_delivery_errors = true
 config.action_mailer.default :charset => "utf-8"
 
   # Compress JavaScripts and CSS.
