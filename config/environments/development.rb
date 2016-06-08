4c3d8b6495e6281e0af800744df92aefd163d419
karthik <karthikrkaranth@gmail.com>
2016-06-08 11:55:52 +0000

    Final commit of the day

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   
 config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'https://peaceful-ocean-80983.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://railsassignment-karthikrk.c9users.io/' }
  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name:      'karanthrkarthik@gmail.com',
